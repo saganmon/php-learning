29c22a30af82110577a23621b913304ca8f4ad20
saganmon <fevers24@gmail.com>
2020-03-22 21:41:12 +0900

    Saved in db by using insert function

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -16,4 +16,21 @@ class QueryBuilder
     $statement->execute(); //クエリの実行
     return $statement->fetchAll(PDO::FETCH_CLASS);
   }
+
+  public function insert($table, $parameters)
+  {
+    $sql = sprintf(
+      'insert into %s (%s) values (%s)', // テーブル名、カラム名、値
+      $table, //テーブル名
+      implode(', ', array_keys($parameters)), // カラム名。array_keysでキーだけ取り出せる
+      ':' . implode(', :' , array_keys($parameters)) //値
+    );
+
+    try {
+      $statement = $this->pdo->prepare($sql);
+      $statement->execute($parameters);  
+    } catch (Exception $e) {
+      die('Whoops, something went wrong.');
+    }
+  }
 }
--- views/index.view.php
+++ views/index.view.php
@@ -1,5 +1,10 @@
 <?php require('partials/head.php'); ?>
 
+<?php foreach ($users as $user) : ?>
+  <li><?= $user->name; ?></li>
+<?php endforeach; ?>
+
+
 <h1>Submit Your Name</h1>
 
 <form method="POST" action="/names">
