3f9a6172b5572d55f67660a9826752f66734453e
saganmon <fevers24@gmail.com>
2020-03-23 00:04:31 +0900

    Composer Autoloading

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require 'vendor/autoload.php'; //composerを使うときにはエントリーポイント(e.g. indexファイル)に必ず入れる！
 require 'core/bootstrap.php'; //DB接続、ルーターなどの準備
 
 require Router::load('routes.php') //loadはインスタンスメソッドなのでインスタンスが必要
